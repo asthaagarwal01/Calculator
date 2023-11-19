869c71953d1e1ef36655a447554f977c3c15f7ff
asthaagarwal01 <astha2agarwal5@gmail.com>
2023-11-19 14:42:20 +0530

    all

---
+++ script.js
@@ -0,0 +1,76 @@
+const keys = document.querySelectorAll(".key");
+const userInput = document.querySelector(".user-input");
+const resetKey = document.querySelector(".reset-key");
+const answerKey = document.querySelector(".answer-key");
+const deleteKey = document.querySelector(".delete-key");
+
+const keysArray = Array.from(keys);
+let lastKeyIsOperator = false; 
+let decimalAdded = false;
+const keyClickHander = (event) => 
+{
+    console.log(event.target.innerText);
+    const value = event.target.innerText; 
+  
+    if (value === "." && decimalAdded) 
+    {
+      return;
+    }
+  
+    if ("+-x/".includes(value)) 
+    {
+      if (lastKeyIsOperator) 
+      {
+        initalValue = userInput.value;
+        updatedValue = initalValue.substring(0, initalValue.length - 1) + value;
+        userInput.value = updatedValue;
+        return;
+      }
+  
+      lastKeyIsOperator = true; 
+      decimalAdded = false; 
+    } 
+    else 
+    {
+      lastKeyIsOperator = false; 
+      if (value === ".") 
+      {
+        decimalAdded = true;
+      }
+    }
+    userInput.value += value; 
+    userInput.scrollLeft = userInput.scrollWidth; 
+  };
+
+  const resetHandler = () => 
+  {
+    userInput.value = "";
+  };
+
+  const deleteHandler = () => 
+  {
+    initalValue = userInput.value;
+    updatedValue = initalValue.substring(0, initalValue.length - 1);
+    userInput.value = updatedValue;
+  };
+  
+  const expressionHandler = (expression) => 
+  {
+    console.log("Inside expression handler");
+    const formattedExpression = expression.replace(/x/g, "*");
+    const result = eval(formattedExpression);
+    return result;
+  };
+  
+  const answerHandler = () => 
+  {
+    console.log("answerClicked");
+    const expression = userInput.value;
+    const result = expressionHandler(expression);
+    userInput.value = result;
+  };
+
+keysArray.forEach((key) => key.addEventListener("click", keyClickHander));
+resetKey.addEventListener("click", resetHandler);
+deleteKey.addEventListener("click", deleteHandler);
+answerKey.addEventListener("click", answerHandler);
